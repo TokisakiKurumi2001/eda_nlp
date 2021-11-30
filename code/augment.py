9bc47218706e830346e87b5388a867237da37841
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2021-11-30 20:32:18 +0700

    Update augment.py

--- code/augment.py
+++ code/augment.py
@@ -64,10 +64,7 @@ def gen_eda(train_orig, model, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_
     ft = fasttext.load_model(model)
 
     for i, line in enumerate(lines):
-        parts = line[:-1].split('\t')
-        label = parts[0]
-        sentence = parts[1]
-        aug_sentences = eda(sentence, ft=ft, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
+        aug_sentences = eda(line, ft=ft, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
         for aug_sentence in aug_sentences:
             writer.write(label + "\t" + aug_sentence + '\n')
 
